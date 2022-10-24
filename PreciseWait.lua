87e5208d77260b2b0ac513ce19c479411da899b5
Bribe <71581275+BribeFromTheHive@users.noreply.github.com>
2022-10-24 22:25:58 +0200

    Update PreciseWait.lua

--- PreciseWait.lua
+++ PreciseWait.lua
@@ -1,5 +1,9 @@
-OnLibraryInit("AddHook", function() -- https://www.hiveworkshop.com/threads/hook.339153
-    --Precise Wait v1.4.1.0
+OnGlobalInit("PreciseWait", function()
+
+    local hook  = Require.optional "AddHook"     --https://www.hiveworkshop.com/threads/hook.339153
+    local remap = Require.optional "GlobalRemap" --https://www.hiveworkshop.com/threads/global-variable-remapper-the-future-of-gui.339308/
+
+    --Precise Wait v1.5.0.0
     --This changes the default functionality of TriggerAddAction, PolledWait
     --and (because they don't work with manual coroutines) TriggerSleepAction and SyncSelections.
     
@@ -17,12 +21,28 @@ OnLibraryInit("AddHook", function() -- https://www.hiveworkshop.com/threads/hook
             coroutine.yield(thread)
         end
     end
+
+    if remap then
+        --This enables GUI to access WaitIndex as a "local" index for their arrays, which allows
+        --the simplest fully-instanciable data attachment in WarCraft 3's GUI history. However,
+        --using it as an array index will cause memory leaks over time, unless you also install
+        --Lua-Infused GUI: https://www.hiveworkshop.com/threads/lua-infused-gui-automatic-group-location-rect-and-force-leak-prevention.317084/
+
+        remap("udg_WaitIndex", coroutine.running)
+    end
+    if not hook then
+        hook = function(varName, userFunc)
+            local old = rawget(_G, varName)
+            rawset(_G, varName, userFunc)
+            return old
+        end
+    end
     
-    AddHook("PolledWait", wait, _WAIT_PRIORITY)
-    AddHook("TriggerSleepAction", wait, _WAIT_PRIORITY)
+    hook("PolledWait", wait, _WAIT_PRIORITY)
+    hook("TriggerSleepAction", wait, _WAIT_PRIORITY)
     
     local oldSync
-    oldSync = AddHook("SyncSelections",
+    oldSync = hook("SyncSelections",
     function()
         local thread = coroutine.running()
         if thread then
@@ -35,20 +55,11 @@ OnLibraryInit("AddHook", function() -- https://www.hiveworkshop.com/threads/hook
         end
     end)
     
-    local oldAdd, init
-    oldAdd = AddHook("TriggerAddAction", function(trig, func)
-        if GlobalRemap and not init then
-            init = true
+    local oldAdd
+    oldAdd = hook("TriggerAddAction", function(trig, func)
 
-            --This enables GUI to access WaitIndex as a "local" index for their arrays, which allows
-            --the simplest fully-instanciable data attachment in WarCraft 3's GUI history. However,
-            --using it as an array index will cause memory leaks over time, unless you also install
-            --the GUI Repair Collection: https://www.hiveworkshop.com/threads/gui-repair-collection.317084/
-
-            GlobalRemap("udg_WaitIndex", coroutine.running)
-        end
         --Return a function that will actually be added as the triggeraction, which itself wraps the actual function in a coroutine.
         return oldAdd(trig, function() coroutine.resume(coroutine.create(func)) end)
     end, _ACTION_PRIORITY)
     
-end) --End of library PolledWait
+end)
